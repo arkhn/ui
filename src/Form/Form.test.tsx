4aa2f426b617e2b583327aee10a6b21fff8c0456
BPierrick <pierrick.bellut95@gmail.com>
2021-03-02 16:09:36 +0100

    Fix: Form builder unit-tests (#42)

---
+++ src/Form/Form.test.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Form from "./Form";
+
+//MutationObserver fix
+import "mutationobserver-shim";
+global.MutationObserver = window.MutationObserver;
+
+type FormData = {
+  name: string;
+};
+
+const submit = jest.fn();
+
+beforeEach(submit.mockClear);
+
+describe("Form builder tests", () => {
+  it("should call submit (internal submit button)", async () => {
+    render(
+      <Form<FormData>
+        properties={[
+          {
+            type: "text",
+            name: "name"
+          }
+        ]}
+        submit={submit}
+      />
+    );
+
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+      expect(submit).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("should call submit (external submit button)", async () => {
+    render(
+      <Form<FormData>
+        properties={[
+          {
+            type: "text",
+            name: "name"
+          }
+        ]}
+        submit={submit}
+        formFooter={<button type="submit">submit</button>}
+      />
+    );
+
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+      expect(submit).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("should submit with data", async () => {
+    render(
+      <Form<FormData>
+        properties={[
+          {
+            type: "text",
+            name: "name"
+          }
+        ]}
+        submit={submit}
+      />
+    );
+
+    userEvent.type(screen.getByRole("textbox"), "hello world");
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+      expect(submit).toHaveBeenCalledTimes(1);
+    });
+    expect(submit).toHaveBeenCalledWith(
+      { name: "hello world" },
+      expect.anything()
+    );
+  });
+
+  it("should invalidate form", async () => {
+    render(
+      <Form<FormData>
+        properties={[
+          {
+            type: "text",
+            name: "name",
+            validationRules: {
+              required: true
+            }
+          }
+        ]}
+        submit={submit}
+        displaySubmitButton
+      />
+    );
+
+    userEvent.click(screen.getByRole("button"));
+
+    const input = screen.getByRole("textbox");
+    await waitFor(() => {
+      expect(input).toBeInvalid;
+    });
+    expect(submit).toHaveBeenCalledTimes(0);
+  });
+});
